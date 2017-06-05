4dd07a368448a0de3ba78d1077d9cfe09ebf26f8
Angel Ortega <an22ortega@gmail.com>
2017-06-04 19:35:09 -0700

    Client-server model fixture

---
+++ src/main/scala/client/Client.scala
@@ -0,0 +1,8 @@
+package client
+
+/**
+  * Created by AngelOrtega on 6/4/2017.
+  */
+trait Client {
+
+}
---
+++ src/main/scala/server/Server.scala
@@ -0,0 +1,8 @@
+package server
+
+/**
+  * Created by AngelOrtega on 6/4/2017.
+  */
+trait Server {
+
+}
