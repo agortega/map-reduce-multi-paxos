146e0cb2ab33b7d3f70ed6b8c130733e06229fdb
Angel Ortega <an22ortega@gmail.com>
2017-06-11 08:46:22 -0700

    akka-acceptor

---
+++ src/main/scala/basicpaxos/BasicPaxos.scala
@@ -0,0 +1,17 @@
+package basicpaxos
+
+//import akka.actor._
+
+object BasicPaxos extends App {
+
+  //val system = ActorSystem("basicpaxos")
+
+//  val acceptors = Seq(
+  //  system.actorOf(Props(new Acceptor(port)), name = "acceptor1"),
+    //system.actorOf(Props(new Acceptor(port)), name = "acceptor2"),
+ //   system.actorOf(Props(new Acceptor(port)), name = "acceptor3"),
+ //   system.actorOf(Props(new Acceptor(port)), name = "acceptor4"),
+ //   system.actorOf(Props(new Acceptor(port)), name = "acceptor5")
+ // )
+
+}
