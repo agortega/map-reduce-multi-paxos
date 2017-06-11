5c8b7f1cc04c524ca92faeeaa5093d668bf69165
Angel Ortega <an22ortega@gmail.com>
2017-06-11 10:06:51 -0700

    partial working in actor system but requests are not sent (dead letters)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,47 @@
+import akka.actor.{Actor, ActorSystem, Address, Deploy, Props}
+import akka.remote._
+import com.typesafe.config.ConfigFactory
+//import akka.testkit.{ AkkaSpec, ImplicitSender }
+
+
 object Main extends App {
   val x = 10
-  println(x)
+//  lazy val oursystem = ActorSystem.
+  val config = ConfigFactory.load()
+  val system = ActorSystem("sampleActorSystem",
+    config.getConfig("akka").withFallback(config))
+//  val actor = system.actorOf(Props[SampleActor], "sampleActor")
+//  actor ! "Pretty slick"
+
+  val addressTwo = Address("akka.tcp", "sys", "127.0.0.1", 5003) // this gives the same
+  val ref = system.actorOf(Props[SampleActor].
+    withDeploy(Deploy(scope = RemoteScope(addressTwo))))
+  ref ! "HELLO"
+  val path = ref.path
+  println(path.address)
+
 }
+
+class EchoActor extends Actor {
+  def receive = {
+    case msg => sender() ! msg
+  }
+}
+class SampleActor extends Actor {
+  def receive = { case _ => sender() ! self }
+}
+
+//def remoteConfig(port: Int) = ConfigFactory.parseString(s"""
+//akka {
+//  actor.provider = "akka.remote.RemoteActorRefProvider"
+//  remote {
+//    enabled-transports = ["akka.remote.netty.tcp"]
+//    netty.tcp {
+//      hostname = "127.0.0.1"
+//      port = $port
+//    }
+//  }
+//}
+//  """)
+//
+//def remotingSystem(name: String, port: Int) = ActorSystem(name, remotingConfig(port))
