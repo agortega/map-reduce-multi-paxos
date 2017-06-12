2a73b1942dd0de4b6090bcfa0a358d75d8bcf0ab
Angel Ortega <an22ortega@gmail.com>
2017-06-11 21:17:31 -0700

    adding some resources for algorithm deployment

---
+++ PotentialResources.md
@@ -0,0 +1,5 @@
+// http://www.lightbend.com/activator/template/akka-distributed-workers
+// http://www.lightbend.com/activator/template/akka-distributed-workers-java
+// http://www.lightbend.com/activator/download
+// http://getakka.net/docs/clustering/cluster-overview
+// https://github.com/agortega/aws-leader-election
--- src/main/scala/basicpaxos/BasicPaxos.scala
+++ src/main/scala/basicpaxos/BasicPaxos.scala
@@ -1,17 +1,15 @@
 package basicpaxos
 
+
+//object BasicPaxos extends App {
+//
+//}
 //import akka.actor._
-
-object BasicPaxos extends App {
-
-  //val system = ActorSystem("basicpaxos")
-
+//val system = ActorSystem("basicpaxos")
 //  val acceptors = Seq(
-  //  system.actorOf(Props(new Acceptor(port)), name = "acceptor1"),
-    //system.actorOf(Props(new Acceptor(port)), name = "acceptor2"),
- //   system.actorOf(Props(new Acceptor(port)), name = "acceptor3"),
- //   system.actorOf(Props(new Acceptor(port)), name = "acceptor4"),
- //   system.actorOf(Props(new Acceptor(port)), name = "acceptor5")
- // )
-
-}
+//  system.actorOf(Props(new Acceptor(port)), name = "acceptor1"),
+//system.actorOf(Props(new Acceptor(port)), name = "acceptor2"),
+//   system.actorOf(Props(new Acceptor(port)), name = "acceptor3"),
+//   system.actorOf(Props(new Acceptor(port)), name = "acceptor4"),
+//   system.actorOf(Props(new Acceptor(port)), name = "acceptor5")
+// )
